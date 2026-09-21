24a713619a448e51c01219ef4868a01257c781b3
Thevalkyrie8 <valkyriecreation8@gmail.com>
2026-09-21 15:53:03 +0700

    Update vite base and build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Website_plantShop/', // <-- THÊM DÒNG NÀY VÀO ĐÂY
+
   plugins: [react()],
 
   server: {
